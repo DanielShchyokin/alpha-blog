3445a89f5afd7bab8083babe32e632799950bccf
dan the cranky tester <bawlscom@gmail.com>
2017-02-25 23:01:49 +0000

    added login form and fixed a bug in users controller dealing with page size

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   get 'signup', to: 'users#new'
   #post 'users', to: "users#create"
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
